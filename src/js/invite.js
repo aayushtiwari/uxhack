93359e76b2ed8376099ec1966741d1df62cb1541
aayushtiwari <arsenicaayush@gmail.com>
2017-07-04 12:49:08 +0530

    upadted

--- src/js/invite.js
+++ src/js/invite.js
@@ -1,13 +1,21 @@
-
-var x=0;
+AOS.init();
+var x=1;
 setInterval(
 ()=>{
 if(x===0){
-  $('.screen')[0].src="src/images/screen1.png";
+  $('.screen')[0].src="src/images/croped1.png";
   x=1;
 }
-else{
- $('.screen')[0].src="src/images/screen2.png";
+else if(x===1){
+ $('.screen')[0].src="src/images/croped2.png";
+  x=2;
+}
+else if(x===2){
+ $('.screen')[0].src="src/images/croped3.png";
+  x=3;
+}
+else if(x===3){
+ $('.screen')[0].src="src/images/croped4.png";
   x=0;
 }
 }
@@ -28,4 +36,44 @@ $('.aboveimg')[0].src="src/images/3.svg";
 });
 $('.b4').hover(()=>{
 $('.aboveimg')[0].src="src/images/4.svg";
+});
+var y=0;
+
+$(window).on('scroll',debounce(check));
+ function debounce(func, wait =20, immediate = true) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    };
+
+function check(){
+if(window.scrollY>960){
+  if(y===0){
+    y=1;
+$('header').css({"position":"fixed"});
+$('header').css({"background-color":"#fff"});
+}
+}
+else{
+  if(y===1){
+    y=0;
+  $('header').css({"position":"static"});
+$('header').css({"background-color":"transparent"});
+}
+}
+};
+var x=0;
+$('#logo div img').on('click',function(){
+  $("#nav div:nth-child(1),#nav div:nth-child(2)").toggleClass("show")
+  $(this).css({'transform':`rotate(${x+90}deg)`});
+  x=x+90;
 });
